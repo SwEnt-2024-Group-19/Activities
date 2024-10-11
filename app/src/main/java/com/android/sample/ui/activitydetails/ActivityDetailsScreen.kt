e1ebd7bbccaeb54de7cc84fa558cb0ba2ff3d8c7
FreezDevo <mohamedtahaguelzim@gmail.com>
2024-10-12 01:01:38 +0200

    refactor: add bottomNav, use Timestamps in ActivityDetailsScreen
      * convert date from String to Timestamp
    
      * add bottomNavBar
    
      * maxPlaces feature remains very fragile, and needs testing

--- app/src/main/java/com/android/sample/ui/activitydetails/ActivityDetailsScreen.kt
+++ app/src/main/java/com/android/sample/ui/activitydetails/ActivityDetailsScreen.kt
@@ -39,133 +39,164 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.android.sample.model.activity.Activity
-import com.android.sample.model.activity.ActivityRepositoryFirestore
 import com.android.sample.model.activity.ActivityStatus
 import com.android.sample.model.activity.ListActivitiesViewModel
-import com.android.sample.model.activity.ListActivityViewModel
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Screen
+import java.util.Calendar
+import java.util.GregorianCalendar
+import kotlin.math.max
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ActivityDetailsScreen(
-    listActivityViewModel: ListActivityViewModel =
-        viewModel(factory = ListActivitiesViewModel.Factory),
-    activityRepositoryFirestore: ActivityRepositoryFirestore,
+    listActivityViewModel: ListActivitiesViewModel = viewModel(factory = ListActivitiesViewModel.Factory),
     navigationActions: NavigationActions
 ) {
-  val activity = listActivityViewModel.selectedActivity.collectAsState().value
-  var activityTitle by remember { mutableStateOf(activity?.title) }
-  var description by remember { mutableStateOf(activity?.description) }
-  var price by remember { mutableStateOf(activity?.price) }
-  var schedule by remember { mutableStateOf(activity?.date) }
-  var location by remember { mutableStateOf(activity?.location) }
-  var placesLeft by remember { mutableStateOf(activity?.placesLeft) }
-  var maxPlaces by remember { mutableStateOf(activity?.maxPlaces) }
-  var creator by remember { mutableStateOf(activity?.creator) }
-  var status by remember { mutableStateOf(activity?.status) }
-
-  val context = LocalContext.current
-
-  Scaffold(
-      topBar = {
-        TopAppBar(
-            title = { Text("Title", modifier = Modifier.testTag("editTodoTitle")) },
-            navigationIcon = {
-              IconButton(
-                  modifier = Modifier.testTag("goBackButton"),
-                  onClick = { navigationActions.goBack() }) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Back")
-                  }
+    val activity = listActivityViewModel.selectedActivity.collectAsState().value
+    val activityTitle by remember { mutableStateOf(activity?.title) }
+    val description by remember { mutableStateOf(activity?.description) }
+    val price by remember { mutableStateOf(activity?.price) }
+    val dueDate by remember {
+        mutableStateOf(
+            activity?.date.let {
+                val calendar = GregorianCalendar()
+                if (activity != null) {
+                    calendar.time = activity.date.toDate()
+                }
+                return@let "${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH) + 1}/${
+                    calendar.get(
+                        Calendar.YEAR
+                    )
+                }"
             })
-      }) { padding ->
-        Column(modifier = Modifier.fillMaxSize().padding(padding).padding(16.dp)) {
-          // Image section
-          Imagery()
+    }
+    val location by remember { mutableStateOf(activity?.location) }
+    val placesLeft by remember { mutableStateOf(activity?.placesLeft) }
+    val maxPlaces by remember { mutableStateOf(activity?.maxPlaces) }
+    val creator by remember { mutableStateOf(activity?.creator) }
+    val status by remember { mutableStateOf(activity?.status) }
 
-          Spacer(modifier = Modifier.height(16.dp))
+    val context = LocalContext.current
 
-          // Title and description
-          Text(text = activityTitle ?: "", style = MaterialTheme.typography.titleMedium)
-          Spacer(modifier = Modifier.height(8.dp))
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Title", modifier = Modifier.testTag("editTodoTitle")) },
+                navigationIcon = {
+                    IconButton(
+                        modifier = Modifier.testTag("goBackButton"),
+                        onClick = { navigationActions.goBack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                })
+        }) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            // Image section
+            Imagery()
 
-          Text(
-              text = "Description:",
-              style = MaterialTheme.typography.bodyMedium,
-          )
-          Spacer(modifier = Modifier.height(4.dp))
-          Text(text = description ?: "", style = MaterialTheme.typography.bodyMedium)
+            Spacer(modifier = Modifier.height(16.dp))
 
-          Spacer(modifier = Modifier.height(16.dp))
+            // Title and description
+            Text(text = activityTitle ?: "", style = MaterialTheme.typography.titleMedium)
+            Spacer(modifier = Modifier.height(8.dp))
 
-          //  price and schedule
-          Spacer(modifier = Modifier.height(8.dp))
-          Row(
-              verticalAlignment = Alignment.CenterVertically,
-              horizontalArrangement = Arrangement.SpaceBetween,
-              modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "Description:",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = description ?: "", style = MaterialTheme.typography.bodyMedium)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            //  price and schedule
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                  Icon(Icons.Filled.AttachMoney, contentDescription = "Price")
-                  Spacer(modifier = Modifier.width(4.dp))
-                  Text(text = if (price != null) "${price.toString()} CHF" else "not defined yet")
+                    Icon(Icons.Filled.AttachMoney, contentDescription = "Price")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = if (price != null) "${price.toString()} CHF" else "not defined yet")
                 }
-              }
-          Spacer(modifier = Modifier.height(8.dp))
-          Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(Icons.Default.DateRange, contentDescription = "Schedule")
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(text = schedule ?: "not defined yet")
-          }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(Icons.Default.DateRange, contentDescription = "Schedule")
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = dueDate ?: "not defined yet")
+            }
 
-          Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(32.dp))
 
-          // Enroll button
-          Button(
-              onClick = {
-                if ((placesLeft ?: 0) > 0) {
-                  val theActivity =
-                      Activity(
-                          uid = activityRepositoryFirestore.getNewUid(),
-                          title = activityTitle ?: "",
-                          description = description ?: "",
-                          date = schedule ?: "",
-                          price = price ?: 0.0,
-                          placesLeft = (placesLeft ?: 0).let { if (it > 0) it - 1 else 0 },
-                          maxPlaces = maxPlaces ?: 0,
-                          creator = creator ?: "",
-                          status = status ?: ActivityStatus.ACTIVE,
-                          location = location ?: "",
-                          images = listOf(),
-                      )
-                  listActivityViewModel.addActivity(theActivity)
-                  Toast.makeText(context, "Enroll Successful", Toast.LENGTH_SHORT).show()
-                  navigationActions.navigateTo(Screen.OVERVIEW)
-                } else {
-                  Toast.makeText(
-                          context, "Enroll failed, limit of places reached", Toast.LENGTH_SHORT)
-                      .show()
-                }
-              },
-              modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp)) {
+            // Enroll button
+            Button(
+                onClick = {
+                    if ((placesLeft ?: 0) > 0) {
+                        val theActivity =
+                            activity?.let { activity ->
+                                Activity(
+                                    uid = listActivityViewModel.getNewUid(),
+                                    title = activityTitle ?: "",
+                                    description = description ?: "",
+                                    date = activity.date,
+                                    price = price ?: 0.0,
+                                    placesLeft = max((placesLeft ?: 0) - 1, 0),
+                                    maxPlaces = maxPlaces ?: 0,
+                                    creator = creator ?: "",
+                                    status = status ?: ActivityStatus.ACTIVE,
+                                    location = location ?: "",
+                                    images = listOf(),
+                                )
+                            }
+                        if (theActivity != null) {
+                            listActivityViewModel.updateActivity(theActivity)
+                        }
+                        Toast.makeText(context, "Enroll Successful", Toast.LENGTH_SHORT).show()
+                        navigationActions.navigateTo(Screen.OVERVIEW)
+                    } else {
+                        Toast.makeText(
+                            context, "Enroll failed, limit of places reached", Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+            ) {
                 Text(text = "Enroll")
-              }
+            }
         }
-      }
+    }
 }
 
 @Composable
 fun Imagery() {
-  LazyRow(
-      modifier = Modifier.fillMaxWidth().height(200.dp).padding(3.dp),
-  ) {
-    items(com.android.sample.ui.activity.items.size) { index ->
-      Image(
-          painter = painterResource(id = com.android.sample.ui.activity.items[index].imageResId),
-          contentDescription = com.android.sample.ui.activity.items[index].contentDescription,
-          contentScale = ContentScale.Crop,
-          modifier = Modifier.padding(8.dp))
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(3.dp),
+    ) {
+        items(com.android.sample.ui.activity.items.size) { index ->
+            Image(
+                painter = painterResource(id = com.android.sample.ui.activity.items[index].imageResId),
+                contentDescription = com.android.sample.ui.activity.items[index].contentDescription,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
     }
-  }
 }
