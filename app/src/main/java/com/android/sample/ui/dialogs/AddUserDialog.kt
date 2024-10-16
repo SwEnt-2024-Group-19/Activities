0a2cbf4ee516dc1003966b994567de13c43e3a5c
Yassyass181104 <yassine.alaoui@epfl.ch>
2024-10-16 21:43:01 +0200

    add:add dialogs with tests

--- app/src/main/java/com/android/sample/ui/dialogs/AddUserDialog.kt
+++ app/src/main/java/com/android/sample/ui/dialogs/AddUserDialog.kt
@@ -1,18 +1,27 @@
 package com.android.sample.ui.dialogs
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
 data class SimpleUser(
     val name: String,
@@ -25,38 +34,59 @@ fun AddUserDialog(
     onDismiss: () -> Unit,
     onAddUser: (SimpleUser) -> Unit,
 ) {
-    Column (
-        modifier = Modifier.fillMaxSize().padding(50.dp),
-    ){
-        var name by remember { mutableStateOf("") }
-        var surname by remember { mutableStateOf("") }
-        var age by remember { mutableStateOf("") }
-
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Name") },
-        )
-
-        TextField(
-            value = surname,
-            onValueChange = { surname = it },
-            label = { Text("Surname") },
-        )
-
-        TextField(
-            value = age,
-            onValueChange = { age = it },
-            label = { Text("Age") },
-        )
-
-        Button(
-            onClick = {
-                onAddUser(SimpleUser(name, surname, age.toInt()))
-                onDismiss()
-            }
+  Dialog(
+      onDismissRequest = onDismiss,
+      properties =
+          DialogProperties(
+              dismissOnBackPress = true,
+              dismissOnClickOutside = true,
+          )) {
+        Column(
+            modifier =
+                Modifier.fillMaxWidth()
+                    .height(300.dp)
+                    .background(color = Color.White, shape = RoundedCornerShape(size = 16.dp)),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text("Add user")
+          var name by remember { mutableStateOf("") }
+          var surname by remember { mutableStateOf("") }
+          var age by remember { mutableStateOf("") }
+
+          OutlinedTextField(
+              modifier = Modifier.testTag("nameTextFieldUser"),
+              value = name,
+              onValueChange = { name = it },
+              label = { Text("Name") },
+          )
+
+          OutlinedTextField(
+              modifier = Modifier.testTag("surnameTextFieldUser"),
+              value = surname,
+              onValueChange = { surname = it },
+              label = { Text("Surname") },
+          )
+          OutlinedTextField(
+              modifier = Modifier.testTag("ageTextFieldUser"),
+              value = age,
+              onValueChange = { age = it },
+              label = { Text("Age") },
+          )
+          Spacer(modifier = Modifier.height(16.dp))
+          Button(
+              modifier = Modifier.testTag("addUserButton"),
+              onClick = {
+                val userAge =
+                    try {
+                      age.toInt()
+                    } catch (e: NumberFormatException) {
+                      0
+                    }
+                onAddUser(SimpleUser(name, surname, userAge))
+                onDismiss()
+              }) {
+                Text("Add user")
+              }
         }
-    }
+      }
 }
