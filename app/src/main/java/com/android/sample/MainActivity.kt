a479886e946dcb3fc16f12f92e86e4857c5b3508
hmaryam84 <maryam.harakat2@gmail.com>
2024-10-17 00:57:03 +0200

    feat: pass navigationAction and listActivitiesViewModel to profile screen and edit profileScreen

--- app/src/main/java/com/android/sample/MainActivity.kt
+++ app/src/main/java/com/android/sample/MainActivity.kt
@@ -28,6 +28,7 @@ import com.android.sample.ui.listActivities.ListActivitiesScreen
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Route
 import com.android.sample.ui.navigation.Screen
+import com.android.sample.ui.profile.EditProfileScreen
 import com.android.sample.ui.profile.ProfileCreationScreen
 import com.android.sample.ui.profile.ProfileScreen
 import com.android.sample.ui.theme.SampleAppTheme
@@ -98,8 +99,12 @@ fun ActivitiesApp(uid: String) {
     }
 
     navigation(startDestination = Screen.PROFILE, route = Route.PROFILE) {
-      composable(Screen.PROFILE) { ProfileScreen(profileViewModel) }
-      composable(Screen.EDIT_PROFILE) { BlankScreen() }
+      composable(Screen.PROFILE) {
+        ProfileScreen(profileViewModel, navigationActions, listActivitiesViewModel)
+      }
+      composable(Screen.EDIT_PROFILE) {
+        EditProfileScreen(profileViewModel, navigationActions, listActivitiesViewModel)
+      }
     }
   }
 }
