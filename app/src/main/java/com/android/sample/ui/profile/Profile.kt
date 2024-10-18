5e8e481a015f2a1b35b32a3211679a84079833ca
ghaliberbich <ghali.berbich22@gmail.com>
2024-10-18 07:00:59 +0200

    feature(edit): Added the button in profile to go to Edit Profile
    Added it because it was deleted after a merge.

--- app/src/main/java/com/android/sample/ui/profile/Profile.kt
+++ app/src/main/java/com/android/sample/ui/profile/Profile.kt
@@ -18,7 +18,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.filled.ModeEdit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -86,8 +88,11 @@ fun LoadingScreen(navigationActions: NavigationActions) {
                           imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
                           contentDescription = "Back")
                   }
-              })
-      }){innerPadding ->
+              })}
+      , floatingActionButton = {
+          FloatingActionButton(onClick = { navigationActions.navigateTo(Screen.EDIT_PROFILE) }) {
+              Icon(Icons.Filled.ModeEdit, contentDescription = "Edit Profile")
+          }}){innerPadding ->
       Column(Modifier.fillMaxSize().padding(innerPadding),
           horizontalAlignment = Alignment.CenterHorizontally){
       Text("You do not have a profile", modifier = Modifier.testTag("loadingText"), color = Color.Black)
