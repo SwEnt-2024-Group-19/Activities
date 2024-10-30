22078af54be4482dac9b3d48e68500b42286b9c4
ghaliberbich <ghali.berbich22@gmail.com>
2024-10-30 20:50:38 +0100

    fix(profile launch): loading of the profile screen works after creation of profile
     synchronised the fetching of profile's data when going to the profile screen just after creating the profile. Before it was needed to relaunch the app to see the profile after having creating the profile.

--- app/src/main/java/com/android/sample/ui/profile/Profile.kt
+++ app/src/main/java/com/android/sample/ui/profile/Profile.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -49,6 +50,8 @@ import com.android.sample.ui.navigation.BottomNavigationMenu
 import com.android.sample.ui.navigation.LIST_TOP_LEVEL_DESTINATION
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Screen
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun ProfileScreen(
@@ -57,6 +60,9 @@ fun ProfileScreen(
     listActivitiesViewModel: ListActivitiesViewModel
 ) {
 
+    LaunchedEffect  (Unit){
+        userProfileViewModel.fetchUserData(FirebaseAuth.getInstance().currentUser?.uid ?: "")
+    }
   val profileState = userProfileViewModel.userState.collectAsState()
 
   when (val profile = profileState.value) {
