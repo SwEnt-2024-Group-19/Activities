58038d10662ae228293aadc0553c9a574dea679c
FreezDevo <mohamedtahaguelzim@gmail.com>
2024-10-31 23:54:03 +0100

    feature(comment): add Comment in creating and editing activites

--- app/src/main/java/com/android/sample/ui/activity/CreateActivity.kt
+++ app/src/main/java/com/android/sample/ui/activity/CreateActivity.kt
@@ -327,7 +327,9 @@ fun CreateActivityScreen(
                             location = location,
                             images = carouselItems.map { it },
                             participants = attendees,
-                            type = types.find { it.name == selectedOption } ?: types[0])
+                            type = types.find { it.name == selectedOption } ?: types[0],
+                            comments = listOf()
+                        )
                     listActivityViewModel.addActivity(activity)
                     profileViewModel.addActivity(creator, activity.uid)
                     navigationActions.navigateTo(Screen.OVERVIEW)
