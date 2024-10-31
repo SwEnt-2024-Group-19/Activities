04d31d2e7dff3c061a495f224a11f78a50b40e5f
ghaliberbich <ghali.berbich22@gmail.com>
2024-10-31 13:20:55 +0100

    refactor(button in profile): use material3 instead of material library

--- app/src/main/java/com/android/sample/ui/profile/Profile.kt
+++ app/src/main/java/com/android/sample/ui/profile/Profile.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
+import androidx.compose.material3.Button
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.filled.ModeEdit
