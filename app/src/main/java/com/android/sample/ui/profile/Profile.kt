4585c825e9ade323963a70f7a9003423c829ad6d
ghaliberbich <ghali.berbich22@gmail.com>
2024-10-31 10:42:00 +0100

    fix(profile launch): loading of the profile screen works after creation of profile and tests pass
     added the fetching of profile's data just after creating the profile. All the tests passed as opposed with the previous solution.

--- app/src/main/java/com/android/sample/ui/profile/Profile.kt
+++ app/src/main/java/com/android/sample/ui/profile/Profile.kt
@@ -14,10 +14,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.filled.ModeEdit
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -26,7 +26,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -50,7 +49,6 @@ import com.android.sample.ui.navigation.BottomNavigationMenu
 import com.android.sample.ui.navigation.LIST_TOP_LEVEL_DESTINATION
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Screen
-import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun ProfileScreen(
@@ -59,9 +57,6 @@ fun ProfileScreen(
     listActivitiesViewModel: ListActivitiesViewModel
 ) {
 
-  LaunchedEffect(Unit) {
-    userProfileViewModel.fetchUserData(FirebaseAuth.getInstance().currentUser?.uid ?: "")
-  }
   val profileState = userProfileViewModel.userState.collectAsState()
 
   when (val profile = profileState.value) {
