155a1ca0a5d123e58dc93fab99d9f69e4570db01
elvan03 <elvankonukseven0@gmail.com>
2024-10-31 23:06:10 +0100

    feat(map): add map screen and navigation

---
+++ app/src/main/java/com/android/sample/ui/map/Map.kt
@@ -0,0 +1,57 @@
+package com.android.sample.ui.map
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.android.sample.ui.navigation.BottomNavigationMenu
+import com.android.sample.ui.navigation.LIST_TOP_LEVEL_DESTINATION
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.navigation.Route
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
+
+@Composable
+fun MapScreen(navigationActions: NavigationActions) {
+    //  val activities by locationViewModel.locationSuggestions.collectAsState()
+
+    Scaffold(
+        content = { pd ->
+            val defaultLocation = LatLng(37.7749, -122.4194)
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(defaultLocation, 10f)
+            }
+
+            GoogleMap(
+                modifier = Modifier.fillMaxSize().padding(pd).testTag("mapScreen"),
+                cameraPositionState = cameraPositionState) {
+                /*activities
+                   .filter { it.location != null }
+                   .forEach { item ->
+                       item.location?.let { LatLng(item.location!!.latitude, it.longitude) }
+                           ?.let {
+                               MarkerState(
+                                   position = it
+                               )
+                           }?.let {
+                               Marker(
+                                   state = it,
+                                   title = item.title,
+                                   snippet = item.description,
+                               )
+                           }
+                   }
+                */
+            }
+        },
+        bottomBar = {
+            BottomNavigationMenu(
+                onTabSelect = { route -> navigationActions.navigateTo(route) },
+                tabList = LIST_TOP_LEVEL_DESTINATION,
+                selectedItem = Route.MAP)
+        })
+}
+
