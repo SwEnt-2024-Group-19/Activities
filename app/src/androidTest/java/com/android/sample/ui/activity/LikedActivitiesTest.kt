37403b0ac1db5dfdfa415de17a72aeca5cebb7be
hmaryam84 <maryam.harakat2@gmail.com>
2024-11-07 13:33:02 +0100

    test: liked activities
       - these new tests verify that liked activities screen works as intended:
        * when the user is not logged in, there is a message
        indicating it and a button to navigate to sign up
        * when the user is logged in but has no liked activity,
        there is a text indicating it
        * when the user is logged in and has liked some activities,
        they are displayed with the same format as in the view list
        activity :( image, title, description, number of places, location),
        the hearts are black since the activities are liked
        * when a user dislikes an activity, the heart becomes
        gray and if the page is refreshed, the activity disappears

---
+++ app/src/androidTest/java/com/android/sample/ui/activity/LikedActivitiesTest.kt
@@ -0,0 +1,201 @@
+package com.android.sample.ui.activity
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.android.sample.model.activity.ActivitiesRepository
+import com.android.sample.model.activity.Activity
+import com.android.sample.model.activity.ActivityStatus
+import com.android.sample.model.activity.ActivityType
+import com.android.sample.model.activity.ListActivitiesViewModel
+import com.android.sample.model.profile.ProfileViewModel
+import com.android.sample.model.profile.ProfilesRepository
+import com.android.sample.model.profile.User
+import com.android.sample.ui.listActivities.ActivityCard2
+import com.android.sample.ui.listActivities.LikedActivitiesScreen
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.navigation.Route
+import com.android.sample.ui.navigation.Screen
+import com.google.firebase.Timestamp
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.kotlin.whenever
+
+class LikedActivitiesTest {
+
+  @get:Rule private lateinit var activitiesRepository: ActivitiesRepository
+  private lateinit var profilesRepository: ProfilesRepository
+  private lateinit var navigationActions: NavigationActions
+  private lateinit var viewModel: ListActivitiesViewModel
+  private lateinit var profileViewModel: ProfileViewModel
+  private lateinit var testUser: User
+
+  private val testActivity =
+      Activity(
+          uid = "1",
+          title = "Mountain Biking",
+          description = "Exciting mountain biking experience.",
+          date = Timestamp.now(),
+          location = "Hills",
+          creator = "Chris",
+          images = listOf(),
+          price = 10.0,
+          status = ActivityStatus.ACTIVE,
+          type = ActivityType.PRO,
+          placesLeft = 8,
+          maxPlaces = 15,
+          participants = listOf(),
+          duration = "2 hours",
+          startTime = "10:00")
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Before
+  fun setUp() {
+    activitiesRepository = mock(ActivitiesRepository::class.java)
+    profilesRepository = mock(ProfilesRepository::class.java)
+    navigationActions = mock(NavigationActions::class.java)
+    viewModel = ListActivitiesViewModel(activitiesRepository)
+
+    testUser =
+        User(
+            id = "Rola",
+            name = "Amine",
+            surname = "A",
+            photo = "",
+            interests = listOf("Cycling", "Reading"),
+            activities = listOf(),
+            likedActivities = listOf(testActivity.uid))
+    // val userStateFlow = MutableStateFlow(testUser)
+    navigationActions = mock(NavigationActions::class.java)
+
+    // `when`(userProfileViewModel.userState).thenReturn(userStateFlow)
+
+    `when`(navigationActions.currentRoute()).thenReturn(Route.LIKED_ACTIVITIES)
+    // composeTestRule.setContent { ListActivitiesScreen(listActivitiesViewModel, navigationActions,
+    // userProfileViewModel) }
+  }
+
+  @Test
+  fun whenUserNotLoggedIn_thenDisplaysSignInPrompt() {
+    profileViewModel = mock(ProfileViewModel::class.java)
+    // Set user profile to null to simulate a user not logged in
+    whenever(profileViewModel.userState).thenReturn(MutableStateFlow(null))
+
+    composeTestRule.setContent {
+      LikedActivitiesScreen(viewModel, navigationActions, profileViewModel)
+    }
+
+    // Verify sign-in prompt is displayed
+    composeTestRule.onNodeWithTag("notConnectedPrompt").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("signInButton").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("signInButton").performClick()
+    verify(navigationActions).navigateTo(Screen.SIGN_UP)
+  }
+
+  @Test
+  fun whenNoLikedActivities_thenDisplaysEmptyMessage() {
+    profileViewModel = mock(ProfileViewModel::class.java)
+    // Set liked activities to empty to simulate no liked activities
+    testUser = testUser.copy(likedActivities = emptyList())
+    whenever(profileViewModel.userState).thenReturn(MutableStateFlow(testUser))
+
+    composeTestRule.setContent {
+      LikedActivitiesScreen(viewModel, navigationActions, profileViewModel)
+    }
+
+    // Verify empty liked activity message is displayed
+    composeTestRule.onNodeWithText("There is no liked activity yet.").assertIsDisplayed()
+  }
+
+  @Test
+  fun whenLikedActivitiesExist_thenDisplaysActivityCards() {
+    profileViewModel = mock(ProfileViewModel::class.java)
+    composeTestRule.setContent {
+      ActivityCard2(
+          activityId = testActivity.uid,
+          navigationActions = navigationActions,
+          listActivitiesViewModel = viewModel,
+          profileViewModel = profileViewModel,
+          profile = testUser,
+          allActivities = listOf(testActivity))
+    }
+    composeTestRule.onNodeWithTag("activityCard").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Mountain Biking").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Exciting mountain biking experience.").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Hills").assertIsDisplayed()
+    composeTestRule.onNodeWithText("8/15").assertIsDisplayed()
+  }
+
+  @Test
+  fun activityListIsDisplayed() {
+    profileViewModel = mock(ProfileViewModel::class.java)
+    composeTestRule.setContent {
+      ActivityCard2(
+          activityId = testActivity.uid,
+          navigationActions = navigationActions,
+          listActivitiesViewModel = viewModel,
+          profileViewModel = profileViewModel,
+          profile = testUser,
+          allActivities = listOf(testActivity))
+    }
+    composeTestRule.onNodeWithTag("activityCard").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Mountain Biking").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Exciting mountain biking experience.").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Hills").assertIsDisplayed()
+    composeTestRule.onNodeWithText("8/15").assertIsDisplayed()
+  }
+
+  @Test
+  fun likeButtonTogglesCorrectly_whenClicked() {
+    // Initially set activity as liked
+    profileViewModel = ProfileViewModel(profilesRepository)
+    composeTestRule.setContent {
+      ActivityCard2(
+          activityId = testActivity.uid,
+          navigationActions = navigationActions,
+          listActivitiesViewModel = viewModel,
+          profileViewModel = profileViewModel,
+          profile = testUser,
+          allActivities = listOf(testActivity))
+    }
+
+    // Verify initial state is liked
+    composeTestRule.onNodeWithTag("activityCard").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("favoriteIcontrue").assertIsDisplayed()
+
+    // Click on the like button
+    composeTestRule.onNodeWithTag("favoriteIcontrue").performClick()
+
+    // Verify that the like button is toggled
+    composeTestRule.onNodeWithTag("favoriteIconfalse").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("favoriteIconfalse").performClick()
+
+    // Verify that the like button is toggled
+    composeTestRule.onNodeWithTag("favoriteIcontrue").assertIsDisplayed()
+  }
+
+  @Test
+  fun navigateToActivityDetails() {
+    // Initially set activity as liked
+    profileViewModel = ProfileViewModel(profilesRepository)
+    composeTestRule.setContent {
+      ActivityCard2(
+          activityId = testActivity.uid,
+          navigationActions = navigationActions,
+          listActivitiesViewModel = viewModel,
+          profileViewModel = profileViewModel,
+          profile = testUser,
+          allActivities = listOf(testActivity))
+    }
+    composeTestRule.onNodeWithTag("activityCard").performClick()
+    verify(navigationActions).navigateTo(Screen.ACTIVITY_DETAILS)
+  }
+}
--- app/src/main/java/com/android/sample/ui/listActivities/LikedActivities.kt
+++ app/src/main/java/com/android/sample/ui/listActivities/LikedActivities.kt
@@ -25,14 +25,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SegmentedButton
-import androidx.compose.material3.SegmentedButtonDefaults
-import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -50,7 +46,6 @@ import androidx.compose.ui.unit.sp
 import com.android.sample.R
 import com.android.sample.model.activity.Activity
 import com.android.sample.model.activity.ListActivitiesViewModel
-import com.android.sample.model.activity.types
 import com.android.sample.model.profile.ProfileViewModel
 import com.android.sample.model.profile.User
 import com.android.sample.ui.navigation.BottomNavigationMenu
@@ -68,78 +63,74 @@ fun LikedActivitiesScreen(
     profileViewModel: ProfileViewModel,
     modifier: Modifier = Modifier
 ) {
-    val uiState by viewModel.uiState.collectAsState()
-    val profile = profileViewModel.userState.collectAsState().value
-    val allActivities=
-        (uiState as ListActivitiesViewModel.ActivitiesUiState.Success).activities
+  val uiState by viewModel.uiState.collectAsState()
+  val profile = profileViewModel.userState.collectAsState().value
+  val allActivities = (uiState as ListActivitiesViewModel.ActivitiesUiState.Success).activities
 
-    Scaffold(
-        modifier = modifier.testTag("listActivitiesScreen"),
-        bottomBar = {
-            BottomNavigationMenu(
-                onTabSelect = { route -> navigationActions.navigateTo(route) },
-                tabList = LIST_TOP_LEVEL_DESTINATION,
-                selectedItem = navigationActions.currentRoute())
-        }) { paddingValues ->
-        Box(modifier = modifier
-            .fillMaxSize()
-            .padding(paddingValues)) {
-            val likedActivitiesList by remember { mutableStateOf(profile?.likedActivities) }
-            when (uiState) {
-                is ListActivitiesViewModel.ActivitiesUiState.Success -> {
-                    if (profile==null){
-                        Text(
-                            text = "You are not logged in, Login or Register to see your liked activities",
-                            modifier =
-                            Modifier
-                                .padding(8.dp)
-                                .align(Alignment.Center)
-                                .testTag("emptyLikedActivityPrompt"),
-                            color = MaterialTheme.colorScheme.onSurface)
-                        Button(
-                            onClick = { navigationActions.navigateTo(Screen.SIGN_UP) },
-                            modifier = Modifier.testTag("signInButton")) {
-                            Text("Go to Sign In Page")
-                        }
+  Scaffold(
+      modifier = modifier.testTag("likedActivitiesScreen"),
+      bottomBar = {
+        BottomNavigationMenu(
+            onTabSelect = { route -> navigationActions.navigateTo(route) },
+            tabList = LIST_TOP_LEVEL_DESTINATION,
+            selectedItem = navigationActions.currentRoute())
+      }) { paddingValues ->
+        Box(modifier = modifier.fillMaxSize().padding(paddingValues)) {
+          val likedActivitiesList by remember { mutableStateOf(profile?.likedActivities) }
+          when (uiState) {
+            is ListActivitiesViewModel.ActivitiesUiState.Success -> {
+              if (profile == null) {
+                Text(
+                    text = "You are not logged in, Login or Register to see your liked activities",
+                    modifier =
+                        Modifier.padding(8.dp)
+                            .align(Alignment.Center)
+                            .testTag("notConnectedPrompt"),
+                    color = MaterialTheme.colorScheme.onSurface)
+                Button(
+                    onClick = { navigationActions.navigateTo(Screen.SIGN_UP) },
+                    modifier = Modifier.testTag("signInButton")) {
+                      Text("Go to Sign In Page")
                     }
-                    if (likedActivitiesList != null) {
-                        if (likedActivitiesList!!.isEmpty()) {
+              }
+              if (likedActivitiesList != null) {
+                if (likedActivitiesList!!.isEmpty()) {
+                  Text(
+                      text = "There is no liked activity yet.",
+                      modifier =
+                          Modifier.padding(8.dp)
+                              .align(Alignment.Center)
+                              .testTag("emptyLikedActivityPrompt"),
+                      color = MaterialTheme.colorScheme.onSurface)
+                } else {
 
-                                Text(
-                                    text = "There is no liked activity yet.",
-                                    modifier =
-                                    Modifier
-                                        .padding(8.dp)
-                                        .align(Alignment.Center)
-                                        .testTag("emptyLikedActivityPrompt"),
-                                    color = MaterialTheme.colorScheme.onSurface)
-
-                        } else {
-
-                            LazyColumn(
-                                modifier = Modifier
-                                    .padding(paddingValues)
-                                    .fillMaxSize()
-                                    .padding(16.dp)
-                                    .padding(horizontal = 5.dp),
-                                verticalArrangement = Arrangement.spacedBy(16.dp)) {
-
-                                items(likedActivitiesList!!) { activityId ->
-                                    ActivityCard2(activityId =activityId , navigationActions, viewModel,profileViewModel,profile,allActivities)
-                                }
-                            }
+                  LazyColumn(
+                      modifier =
+                          Modifier.padding(paddingValues)
+                              .fillMaxSize()
+                              .padding(16.dp)
+                              .padding(horizontal = 5.dp),
+                      verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                        items(likedActivitiesList!!) { activityId ->
+                          ActivityCard2(
+                              activityId = activityId,
+                              navigationActions,
+                              viewModel,
+                              profileViewModel,
+                              profile,
+                              allActivities)
                         }
-                    }
+                      }
                 }
-                is ListActivitiesViewModel.ActivitiesUiState.Error -> {
-                    val error = (uiState as ListActivitiesViewModel.ActivitiesUiState.Error).exception
-                    Text(text = "Error: ${error.message}", modifier = Modifier.padding(8.dp))
-                }
+              }
             }
-
-
+            is ListActivitiesViewModel.ActivitiesUiState.Error -> {
+              val error = (uiState as ListActivitiesViewModel.ActivitiesUiState.Error).exception
+              Text(text = "Error: ${error.message}", modifier = Modifier.padding(8.dp))
+            }
+          }
         }
-    }
+      }
 }
 
 @Composable
@@ -150,132 +141,114 @@ fun ActivityCard2(
     profileViewModel: ProfileViewModel,
     profile: User?,
     allActivities: List<Activity>,
-
 ) {
 
-    val activity= allActivities.filter{ act->act.uid==activityId}[0]
+  val activity = allActivities.filter { act -> act.uid == activityId }[0]
 
-    val dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
-    val formattedDate = dateFormat.format(activity.date.toDate())
+  val dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+  val formattedDate = dateFormat.format(activity.date.toDate())
 
-    var isLiked by remember { mutableStateOf(profile?.likedActivities?.contains(activity.uid) ?: false) }
+  var isLiked by remember {
+    mutableStateOf(profile?.likedActivities?.contains(activity.uid) ?: false)
+  }
 
-    Card(
-        modifier =
-        Modifier
-            .fillMaxWidth()
-            .testTag("activityCard")
-            .clip(RoundedCornerShape(16.dp))
-            .clickable {
+  Card(
+      modifier =
+          Modifier.fillMaxWidth()
+              .testTag("activityCard")
+              .clip(RoundedCornerShape(16.dp))
+              .clickable {
                 listActivitiesViewModel.selectActivity(activity)
                 navigationActions.navigateTo(Screen.ACTIVITY_DETAILS)
-            },
-        elevation = CardDefaults.cardElevation(8.dp)) {
+              },
+      elevation = CardDefaults.cardElevation(8.dp)) {
         Column {
-            // Box for overlaying the title on the image
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .height(180.dp)) {
-                // Display the activity image
-                Image(
-                    painter = painterResource(R.drawable.foot),
-                    contentDescription = activity.title,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(180.dp),
-                    contentScale = ContentScale.Crop)
+          // Box for overlaying the title on the image
+          Box(modifier = Modifier.fillMaxWidth().height(180.dp)) {
+            // Display the activity image
+            Image(
+                painter = painterResource(R.drawable.foot),
+                contentDescription = activity.title,
+                modifier = Modifier.fillMaxWidth().height(180.dp),
+                contentScale = ContentScale.Crop)
 
-                // Display the activity name on top of the image
-                Text(
-                    text = activity.title,
-                    style =
+            // Display the activity name on top of the image
+            Text(
+                text = activity.title,
+                style =
                     MaterialTheme.typography.titleLarge.copy(
                         fontWeight = FontWeight.Bold,
                         color = Color.White // Title color set to black
-                    ),
-                    modifier =
-                    Modifier
-                        .align(Alignment.BottomStart)
-                        .padding(16.dp)
-                        .testTag("titleActivity"))
-            }
+                        ),
+                modifier = Modifier.align(Alignment.BottomStart).padding(16.dp))
+          }
 
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically) {
+          Spacer(modifier = Modifier.height(8.dp))
+          Row(
+              modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
+              horizontalArrangement = Arrangement.SpaceBetween,
+              verticalAlignment = Alignment.CenterVertically) {
                 // Display the date
                 Text(
                     text = formattedDate,
                     style =
-                    MaterialTheme.typography.bodySmall.copy(
-                        color = Color.Gray, // Light gray color for the date
-                        fontStyle = FontStyle.Italic
-                    ),
+                        MaterialTheme.typography.bodySmall.copy(
+                            color = Color.Gray, // Light gray color for the date
+                            fontStyle = FontStyle.Italic),
                     modifier = Modifier.weight(1f) // Takes up remaining space
-                )
+                    )
 
                 if (profile != null) {
-                    IconButton(
-                        onClick = {
-                            isLiked = !isLiked
-                            if (isLiked) {
-                                profileViewModel.addLikedActivity(profile.id, activity.uid)
-                            } else {
-                                profileViewModel.removeLikedActivity(profile.id, activity.uid)
-                            }
-                        },
-
-                        ) {
-                        Icon(
-                            imageVector = if (isLiked) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
-                            contentDescription = if (isLiked) "Liked" else "Not Liked",
-                            tint = if (isLiked) Color.Black else Color.Gray
-                        )
-
-                    }
+                  IconButton(
+                      modifier = Modifier.testTag("favoriteIcon$isLiked"),
+                      onClick = {
+                        isLiked = !isLiked
+                        if (isLiked) {
+                          profileViewModel.addLikedActivity(profile.id, activity.uid)
+                        } else {
+                          profileViewModel.removeLikedActivity(profile.id, activity.uid)
+                        }
+                      },
+                  ) {
+                    Icon(
+                        imageVector =
+                            if (isLiked) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                        contentDescription = if (isLiked) "Liked" else "Not Liked",
+                        tint = if (isLiked) Color.Black else Color.Gray)
+                  }
                 }
-            }
+              }
 
-            Row(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically) {
+          Row(
+              modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
+              horizontalArrangement = Arrangement.SpaceBetween,
+              verticalAlignment = Alignment.CenterVertically) {
                 // Location on the left
                 Text(
                     text = activity.location,
                     style =
-                    MaterialTheme.typography.bodySmall.copy(
-                        fontStyle = FontStyle.Italic, color = Color.Gray),
+                        MaterialTheme.typography.bodySmall.copy(
+                            fontStyle = FontStyle.Italic, color = Color.Gray),
                     modifier = Modifier.weight(1f) // Takes up remaining space
-                )
+                    )
 
                 Text(
                     text = "${activity.placesLeft}/${activity.maxPlaces}",
                     style =
-                    MaterialTheme.typography.bodyMedium.copy(
-                        fontWeight = FontWeight.SemiBold, color = Color.Gray, fontSize = 16.sp),
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .padding(end = 16.dp))
-            }
+                        MaterialTheme.typography.bodyMedium.copy(
+                            fontWeight = FontWeight.SemiBold, color = Color.Gray, fontSize = 16.sp),
+                    modifier = Modifier.align(Alignment.CenterVertically).padding(end = 16.dp))
+              }
 
-            Spacer(modifier = Modifier.height(4.dp))
+          Spacer(modifier = Modifier.height(4.dp))
 
-            // Display the activity description
-            Text(
-                text = activity.description,
-                style =
-                MaterialTheme.typography.bodyMedium.copy(color = Color.Black, lineHeight = 20.sp),
-                modifier = Modifier.padding(horizontal = 16.dp))
-            Spacer(modifier = Modifier.height(8.dp))
+          // Display the activity description
+          Text(
+              text = activity.description,
+              style =
+                  MaterialTheme.typography.bodyMedium.copy(color = Color.Black, lineHeight = 20.sp),
+              modifier = Modifier.padding(horizontal = 16.dp))
+          Spacer(modifier = Modifier.height(8.dp))
         }
-    }
+      }
 }
-
-
