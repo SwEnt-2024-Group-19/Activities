fac21f3c5a055c56e8c155aaa9a6bde026ce04ec
hmaryam84 <maryam.harakat2@gmail.com>
2024-11-07 20:04:11 +0100

    feat: add like button in activity details
     * now, it is possible to like an activity from the activity details screen
     * a "like" button appears on the activity image
     * the heart is black when the activity is liked by the user
     * the heart becomes gray when the user dislikes the activity

--- app/src/main/java/com/android/sample/ui/activitydetails/ActivityDetailsScreen.kt
+++ app/src/main/java/com/android/sample/ui/activitydetails/ActivityDetailsScreen.kt
@@ -23,6 +23,8 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.AttachMoney
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -47,10 +49,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.android.sample.model.activity.Activity
 import com.android.sample.model.activity.ActivityStatus
 import com.android.sample.model.activity.Comment
 import com.android.sample.model.activity.ListActivitiesViewModel
 import com.android.sample.model.profile.ProfileViewModel
+import com.android.sample.model.profile.User
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Screen
 import com.google.firebase.Timestamp
@@ -159,7 +163,9 @@ fun ActivityDetailsScreen(
                         text = "Activity Image",
                         color = Color.White,
                         modifier = Modifier.align(Alignment.Center))
-                  }
+                  LikeButton(profile, activity, profileViewModel)
+
+              }
 
               // Title
               Box(
@@ -490,3 +496,33 @@ fun CommentItem(
     }
   }
 }
+@Composable
+fun LikeButton(profile: User?, activity: Activity?, profileViewModel: ProfileViewModel) {
+    var isLiked by remember {
+        mutableStateOf(activity?.let { profile?.likedActivities?.contains(it.uid) } ?: false)
+    }
+
+    if (profile != null) {
+        IconButton(
+            modifier = Modifier.testTag("likeButton$isLiked"),
+            onClick = {
+                isLiked = !isLiked
+                if (isLiked) {
+                    if (activity != null) {
+                        profileViewModel.addLikedActivity(profile.id, activity.uid)
+                    }
+                } else {
+                    if (activity != null) {
+                        profileViewModel.removeLikedActivity(profile.id, activity.uid)
+                    }
+                }
+            },
+        ) {
+            Icon(
+                imageVector = if (isLiked) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
+                contentDescription = if (isLiked) "Liked" else "Not Liked",
+                tint = if (isLiked) Color.Black else Color.LightGray)
+        }
+    }
+}
+
