401beff6cc3c9c64fbfd3be0adfb3496beb340f5
Amine Benjelloun Touimi <amine.benjellountouimi@epfl.ch>
2024-11-07 15:02:52 +0100

    feat(location): update the view model with a function calling the repository to get the current location.

--- app/src/main/java/com/android/sample/model/map/LocationViewModel.kt
+++ app/src/main/java/com/android/sample/model/map/LocationViewModel.kt
@@ -1,36 +1,44 @@
 package com.android.sample.model.map
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import okhttp3.OkHttpClient
 
-class LocationViewModel @Inject constructor(private val repository: LocationRepository) :
-    ViewModel() {
+@HiltViewModel
+open class LocationViewModel
+@Inject
+constructor(
+    private val repository: LocationRepository,
+    private val permissionChecker: LocationPermissionChecker
+) : ViewModel() {
   private val query_ = MutableStateFlow("")
   val query: StateFlow<String> = query_
 
   private var locationSuggestions_ = MutableStateFlow(emptyList<Location>())
   val locationSuggestions: StateFlow<List<Location>> = locationSuggestions_
 
-  // create factory
-  companion object {
-    val Factory: ViewModelProvider.Factory =
-        object : ViewModelProvider.Factory {
-          @Suppress("UNCHECKED_CAST")
-          override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return LocationViewModel(NominatimLocationRepository(OkHttpClient())) as T
-          }
-        }
-  }
+  private val _currentLocation = MutableStateFlow<Location?>(null)
+  val currentLocation: StateFlow<Location?> = _currentLocation
 
   fun setQuery(query: String) {
     query_.value = query
 
     if (query.isNotEmpty()) {
       repository.search(query, { locationSuggestions_.value = it }, {})
+    }
+  }
+
+  fun fetchCurrentLocation() {
+    if (permissionChecker.hasLocationPermission()) {
+      repository.getCurrentLocation(
+          onSuccess = { _currentLocation.value = it },
+          onFailure = { Log.e("LocationViewModel", "Failed to get location", it) })
+    } else {
+      Log.e("LocationViewModel", "Location permission not granted")
+      // Handle the permission denial as appropriate
     }
   }
 }
