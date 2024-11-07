62f8b4a141ab7f024143445c360e0412928374d3
hmaryam84 <maryam.harakat2@gmail.com>
2024-11-07 14:40:52 +0100

    test: like activities
      - add a test to check if the like icon changes on click in the good way:
         * becomes black if the user likes
         * becomes white if the user dislikes

--- app/src/androidTest/java/com/android/sample/ui/listActivities/ListActivitiesTest.kt
+++ app/src/androidTest/java/com/android/sample/ui/listActivities/ListActivitiesTest.kt
@@ -12,6 +12,7 @@ import com.android.sample.model.activity.ActivityStatus
 import com.android.sample.model.activity.ActivityType
 import com.android.sample.model.activity.ListActivitiesViewModel
 import com.android.sample.model.profile.ProfileViewModel
+import com.android.sample.model.profile.ProfilesRepository
 import com.android.sample.model.profile.User
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Route
@@ -28,6 +29,7 @@ import org.mockito.kotlin.any
 
 class OverviewScreenTest {
   private lateinit var activitiesRepository: ActivitiesRepository
+  private lateinit var profilesRepository: ProfilesRepository
   private lateinit var navigationActions: NavigationActions
   private lateinit var listActivitiesViewModel: ListActivitiesViewModel
   private lateinit var userProfileViewModel: ProfileViewModel
@@ -55,10 +57,11 @@ class OverviewScreenTest {
 
   @Before
   fun setUp() {
+    profilesRepository = mock(ProfilesRepository::class.java)
     activitiesRepository = mock(ActivitiesRepository::class.java)
     navigationActions = mock(NavigationActions::class.java)
     listActivitiesViewModel = ListActivitiesViewModel(activitiesRepository)
-    userProfileViewModel = mock(ProfileViewModel::class.java)
+
     testUser =
         User(
             id = "Rola",
@@ -68,16 +71,18 @@ class OverviewScreenTest {
             interests = listOf("Cycling", "Reading"),
             activities = listOf(activity.uid),
         )
-    val userStateFlow = MutableStateFlow(testUser)
+
     navigationActions = mock(NavigationActions::class.java)
 
-    `when`(userProfileViewModel.userState).thenReturn(userStateFlow)
+    // `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
 
     `when`(navigationActions.currentRoute()).thenReturn(Route.OVERVIEW)
   }
 
   @Test
   fun displayTextWhenEmpty() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     composeTestRule.setContent {
       ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     }
@@ -91,6 +96,8 @@ class OverviewScreenTest {
 
   @Test
   fun displaysActivity() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     composeTestRule.setContent {
       ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     }
@@ -115,6 +122,8 @@ class OverviewScreenTest {
 
   @Test
   fun segmentedButtonRowIsDisplayed() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     composeTestRule.setContent {
       ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     }
@@ -131,6 +140,8 @@ class OverviewScreenTest {
 
   @Test
   fun displayTextWhenNoSolo() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     composeTestRule.setContent {
       ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     }
@@ -140,15 +151,21 @@ class OverviewScreenTest {
 
   @Test
   fun bottomNavigationMenuIsDisplayed() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     composeTestRule.setContent {
       ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     }
     // Ensure bottom navigation menu is displayed
     composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
+    // composeTestRule.onNodeWithTag("Liked").performClick()
+    // verify(navigationActions).navigateTo(Screen.LIKED_ACTIVITIES)
   }
 
   @Test
   fun goesToDetailsOnClick() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     composeTestRule.setContent {
       ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     }
@@ -168,6 +185,8 @@ class OverviewScreenTest {
 
   @Test
   fun filteringWorks() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     composeTestRule.setContent {
       ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     }
@@ -192,7 +211,9 @@ class OverviewScreenTest {
   }
 
   @Test
-  fun changeIconWhenActivityIsLiked() {
+  fun changeIconWhenActivityNotLiked() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     composeTestRule.setContent {
       ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     }
@@ -205,7 +226,9 @@ class OverviewScreenTest {
   }
 
   @Test
-  fun changeIconWhenActivityIsLiked2() {
+  fun changeIconWhenActivityIsLiked() {
+    userProfileViewModel = mock(ProfileViewModel::class.java)
+    `when`(userProfileViewModel.userState).thenReturn(MutableStateFlow(testUser))
     `when`(activitiesRepository.getActivities(any(), any())).then {
       it.getArgument<(List<Activity>) -> Unit>(0)(listOf(activity))
     }
@@ -221,5 +244,33 @@ class OverviewScreenTest {
     // ListActivitiesScreen(listActivitiesViewModel, navigationActions, userProfileViewModel)
     // }
     composeTestRule.onNodeWithTag("likeButtontrue").assertIsDisplayed()
+  }
+
+  @Test
+  fun changeIconWhenActivityIsOnclick() {
+    userProfileViewModel = ProfileViewModel(profilesRepository)
+
+    composeTestRule.setContent {
+      ActivityCard(
+          navigationActions = navigationActions,
+          listActivitiesViewModel = listActivitiesViewModel,
+          profileViewModel = userProfileViewModel,
+          profile = testUser,
+          activity = activity)
+    }
+
+    // Verify initial state is liked
+    composeTestRule.onNodeWithTag("activityCard").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("likeButtonfalse").assertIsDisplayed()
+
+    // Click on the like button
+    composeTestRule.onNodeWithTag("likeButtonfalse").performClick()
+
+    // Verify that the like button is toggled
+    composeTestRule.onNodeWithTag("likeButtontrue").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("likeButtontrue").performClick()
+
+    // Verify that the like button is toggled
+    composeTestRule.onNodeWithTag("likeButtonfalse").assertIsDisplayed()
   }
 }
