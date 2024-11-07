38371f9ac5d40bf7bb737c2ed9d1fb0e3db0f5d7
Amine Benjelloun Touimi <amine.benjellountouimi@epfl.ch>
2024-11-07 15:53:11 +0100

    format(location): run ktfmtFormat.

--- app/src/main/java/com/android/sample/model/map/LocationRepository.kt
+++ app/src/main/java/com/android/sample/model/map/LocationRepository.kt
@@ -2,5 +2,6 @@ package com.android.sample.model.map
 
 interface LocationRepository {
   fun search(query: String, onSuccess: (List<Location>) -> Unit, onFailure: (Exception) -> Unit)
+
   fun getCurrentLocation(onSuccess: (Location) -> Unit, onFailure: (Exception) -> Unit)
 }
