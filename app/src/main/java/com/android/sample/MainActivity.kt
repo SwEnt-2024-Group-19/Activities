c2df0762edd04b6781ee5ab2e117bc4af5086c81
Amine Benjelloun Touimi <amine.benjellountouimi@epfl.ch>
2024-11-07 14:56:18 +0100

    feat(location): add location permissions check in MainActivity

--- app/src/main/java/com/android/sample/MainActivity.kt
+++ app/src/main/java/com/android/sample/MainActivity.kt
@@ -47,12 +47,20 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   private val CAMERA_PERMISSION_REQUEST_CODE = 0
+  private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
   private lateinit var auth: FirebaseAuth
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     if (!hasCameraPermissions(applicationContext)) {
       ActivityCompat.requestPermissions(this, CAMERAX_PERMISSIONS, CAMERA_PERMISSION_REQUEST_CODE)
+    }
+    Log.d("MainActivity", "Checking location permissions")
+    if (!hasLocationPermissions(applicationContext)) {
+      Log.d("MainActivity", "Requesting location permissions")
+      ActivityCompat.requestPermissions(
+          this, LOCATION_PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE)
     }
     auth = FirebaseAuth.getInstance()
     val currentUser = auth.currentUser
@@ -78,6 +86,17 @@ class MainActivity : ComponentActivity() {
     }
   }
 
+  private fun hasLocationPermissions(context: Context): Boolean {
+    return LOCATION_PERMISSIONS.all {
+      ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+  }
+
+  private val LOCATION_PERMISSIONS =
+      arrayOf(
+          Manifest.permission.ACCESS_FINE_LOCATION,
+          Manifest.permission.ACCESS_COARSE_LOCATION,
+      )
   private val CAMERAX_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 }
 
