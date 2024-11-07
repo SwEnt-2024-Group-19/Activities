7a3d0dbc9c4e86a27e5c4913bbeaaaef2ae8c390
Amine Benjelloun Touimi <amine.benjellountouimi@epfl.ch>
2024-11-07 20:58:55 +0100

    refactor(location): simple syntax change.

--- app/src/main/java/com/android/sample/model/map/LocationPermissionChecker.kt
+++ app/src/main/java/com/android/sample/model/map/LocationPermissionChecker.kt
@@ -1,14 +1,12 @@
 package com.android.sample.model.map
 
-import android.Manifest
 import android.content.Context
-import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
-import javax.inject.Inject
 
-open class LocationPermissionChecker @Inject constructor(private val context: Context) {
+class LocationPermissionChecker(private val context: Context) {
   fun hasLocationPermission(): Boolean {
-    return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
-        PackageManager.PERMISSION_GRANTED
+    return ContextCompat.checkSelfPermission(
+        context, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+        android.content.pm.PackageManager.PERMISSION_GRANTED
   }
 }
