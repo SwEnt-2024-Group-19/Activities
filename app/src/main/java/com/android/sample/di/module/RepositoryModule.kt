497aefd5c756263437a251bdb3dc06ee2c00fc43
FreezDevo <mohamedtahaguelzim@gmail.com>
2024-11-13 01:41:59 +0100

    test: implement PermissionChecker dependency injection
      * PermissionChecker is now injected directly from RepositoryModule
    
      * PermissionModule is no longer needed, as RepositoryModule now does the job

--- app/src/main/java/com/android/sample/di/module/RepositoryModule.kt
+++ app/src/main/java/com/android/sample/di/module/RepositoryModule.kt
@@ -8,6 +8,7 @@ import com.android.sample.model.auth.SignInRepositoryFirebase
 import com.android.sample.model.map.LocationPermissionChecker
 import com.android.sample.model.map.LocationRepository
 import com.android.sample.model.map.NominatimLocationRepository
+import com.android.sample.model.map.PermissionChecker
 import com.android.sample.model.profile.ProfilesRepository
 import com.android.sample.model.profile.ProfilesRepositoryFirestore
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -58,7 +59,7 @@ object RepositoryModule {
   @Singleton
   fun provideLocationPermissionChecker(
       @ApplicationContext context: Context
-  ): LocationPermissionChecker {
+  ): PermissionChecker {
     return LocationPermissionChecker(context)
   }
 
