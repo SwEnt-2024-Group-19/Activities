d7c1ce3466c2cd5b19408bcd7cc6014f175573a1
hmaryam84 <maryam.harakat2@gmail.com>
2024-11-13 01:38:29 +0100

    fix: invalid start time format in edit activity
     * show a toast indicating invalid format of the start time when editing the activity using an invalid start time
     * add the following start time conditions:
      * the hours should be between 00 and 23
      * the minutes should be between 00 and 59
      * there should be no letter

--- app/src/main/java/com/android/sample/ui/activity/EditActivity.kt
+++ app/src/main/java/com/android/sample/ui/activity/EditActivity.kt
@@ -398,7 +398,10 @@ fun EditActivityScreen(
                 enabled = title.isNotEmpty() && description.isNotEmpty() && dueDate.isNotEmpty(),
                 onClick = {
                   val timeFormat = startTime?.split(":") ?: listOf()
-                  if (timeFormat.size != 2) {
+                    val timePattern = Regex("^([01]?[0-9]|2[0-3]):[0-5][0-9]$")
+
+                    if (timeFormat.size != 2 || !startTime?.let { timePattern.matches(it) }!!) {
+
                     Toast.makeText(
                             context, "Invalid format, time must be HH:MM.", Toast.LENGTH_SHORT)
                         .show()
@@ -411,7 +414,11 @@ fun EditActivityScreen(
                   }
                   val calendar = GregorianCalendar()
                   val parts = dueDate.split("/")
-                  if (parts.size == 3) {
+                  if (parts.size == 3 && timeFormat.size == 2 && durationFormat.size == 2 && startTime?.let {
+                          timePattern.matches(
+                              it
+                          )
+                      } == true) {
                     try {
                       calendar.set(
                           parts[2].toInt(),
