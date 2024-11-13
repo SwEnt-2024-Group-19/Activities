d1da11479df542996108727ae68f3c08b86d6f3e
FreezDevo <mohamedtahaguelzim@gmail.com>
2024-11-13 02:00:43 +0100

    test: implement first end-to-end test
      * first end to end that simulates a guest that opens the app and wants to view the details of an activity
    
      * this is our first test that includes multiple screens. It should work well. It tests some features such as filtering activities. It also attempts to open other tabs, and checks whether their behaviour is adapted to guest mode or not.
    
      * this test should be enhanced to have a full guest mode end to end experience. It could include scenarios where the guest is not allowed to create activities, to enroll, or like activities.

--- app/src/androidTest/java/com/android/sample/ui/endtoend/ActivityFlowTest.kt
+++ app/src/androidTest/java/com/android/sample/ui/endtoend/ActivityFlowTest.kt
@@ -1,6 +1,9 @@
 package com.android.sample.ui.endtoend
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.rule.GrantPermissionRule
 import com.android.sample.MainActivity
 import com.android.sample.model.activity.ActivitiesRepository
@@ -13,6 +16,7 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import javax.inject.Inject
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 @HiltAndroidTest
 class ActivityFlowTest {
@@ -38,8 +42,32 @@ class ActivityFlowTest {
           android.Manifest.permission.ACCESS_COARSE_LOCATION,
           android.Manifest.permission.CAMERA)
 
-  @Before
-  fun setUp() {
-    hiltRule.inject()
-  }
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun aGuestTriesToLookAtAnActivity() {
+        // Opens the app as a guest
+        composeTestRule.onNodeWithTag("ContinueAsGuestButton").performClick()
+
+        // Checks that is not connected in profile
+        composeTestRule.onNodeWithTag("Profile").performClick()
+        composeTestRule.onNodeWithTag("loadingText").assertIsDisplayed()
+
+        // Goes back to the main screen and tries to filter activities
+        composeTestRule.onNodeWithTag("Overview").performClick()
+        composeTestRule.onNodeWithTag("listActivitiesScreen").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("segmentedButtonRow").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("segmentedButtonSOLO").performClick()
+        composeTestRule.onNodeWithTag("emptyActivityPrompt").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("segmentedButtonPRO").performClick()
+        composeTestRule.onNodeWithTag("activityCard").assertIsDisplayed()
+
+        // Opens the activity details
+        composeTestRule.onNodeWithTag("activityCard").performClick()
+        composeTestRule.onNodeWithTag("activityDetailsScreen").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("notLoggedInText").assertIsDisplayed()
+    }
 }
