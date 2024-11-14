a496d7b678c757276a116fc1b31ebc2cc0d8cc93
Yassyass181104 <yassine.alaoui@epfl.ch>
2024-11-14 09:57:51 +0100

    add:add constants used in code for UI

--- app/src/main/java/com/android/sample/resources/C.kt
+++ app/src/main/java/com/android/sample/resources/C.kt
@@ -17,5 +17,43 @@ object C {
     val ERROR_TEXTFIELD_PADDING_START = 16.dp
     val ERROR_TEXTFIELD_PADDING_TOP = 5.dp
     val ERROR_TEXTFIELD_FONT_SIZE = 12.sp
+
+    const val STANDARD_PADDING = 8
+
+    const val BUTTON_WIDTH = 300
+
+    const val BUTTON_HEIGHT = 40
+
+    const val MEDIUM_PADDING = 16
+
+    const val LARGE_PADDING = 24
+
+    const val SMALL_PADDING = 4
+
+    const val TITLE_FONTSIZE = 24
+
+    const val SUBTITLE_FONTSIZE = 16
+
+    const val WHITE_COLOR = 0xFFFFFFFF
+
+    const val BLACK_COLOR = 0xFF000000
+
+    const val IMAGE_SIZE = 100
+
+    const val LARGE_IMAGE_SIZE = 200
+
+    const val SMALL_IMAGE_SIZE = 30
+
+    const val MEDIUM_IMAGE_SIZE = 64
+
+    const val BOTTOM_MENU_HEIGHT = 60
+
+    const val BOTTOM_CORNER = 50
+
+    const val TOP_TITLE_SIZE = 30
+
+    const val TEXT_FONTSIZE = 12
+
+    const val WIDTH_FRACTION= 0.8f
   }
 }
