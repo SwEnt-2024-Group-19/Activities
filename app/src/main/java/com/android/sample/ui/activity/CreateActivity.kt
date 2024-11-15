a91893e5e4e0eaa662676008bc27eddc3e3ef480
hmaryam84 <maryam.harakat2@gmail.com>
2024-11-15 08:04:45 +0100

    test: add missing test tags
     * add testags for:
       * the start time
       * the duration

--- app/src/main/java/com/android/sample/ui/activity/CreateActivity.kt
+++ app/src/main/java/com/android/sample/ui/activity/CreateActivity.kt
@@ -249,7 +249,7 @@ fun CreateActivityScreen(
                 value = startTime,
                 onValueChange = { startTime = it },
                 label = { Text("Time") },
-                modifier = Modifier.padding(STANDARD_PADDING.dp).fillMaxWidth(),
+                modifier = Modifier.padding(STANDARD_PADDING.dp).fillMaxWidth().testTag("inputTimeCreate"),
                 placeholder = { Text(text = stringResource(id = R.string.hour_min_format)) },
                 singleLine = true,
             )
@@ -259,7 +259,7 @@ fun CreateActivityScreen(
                 value = duration,
                 onValueChange = { duration = it },
                 label = { Text("Duration") },
-                modifier = Modifier.padding(STANDARD_PADDING.dp).fillMaxWidth(),
+                modifier = Modifier.padding(STANDARD_PADDING.dp).fillMaxWidth().testTag("inputDurationCreate"),
                 placeholder = { Text(text = stringResource(id = R.string.hour_min_format)) },
                 singleLine = true,
             )
