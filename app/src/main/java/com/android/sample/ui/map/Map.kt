e1aa5edefaf6955637c8ed73734522ecbcfaa419
hmaryam84 <maryam.harakat2@gmail.com>
2024-11-18 15:41:57 +0100

    feature: add activity details preview on map
      * Display activity details preview when clicking on a marker:
        * the first image of the activity
        * the title of the activity
        * the description of the activity
        * the price of the activity
        * the date and time of the activity
        * the exact location of the activity
        * the number of places left
    
      * Navigate to activity details screen when clicking on see more details button
    
      * Close the preview when clicking on the close icon button or on dismiss request

--- app/src/main/java/com/android/sample/ui/map/Map.kt
+++ app/src/main/java/com/android/sample/ui/map/Map.kt
@@ -6,24 +6,40 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.MyLocation
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import coil.compose.AsyncImage
+import com.android.sample.model.activity.Activity
 import com.android.sample.model.activity.ListActivitiesViewModel
 import com.android.sample.model.map.LocationViewModel
 import com.android.sample.ui.navigation.BottomNavigationMenu
 import com.android.sample.ui.navigation.LIST_TOP_LEVEL_DESTINATION
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Route
+import com.android.sample.ui.navigation.Screen
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
@@ -31,104 +47,246 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapScreen(
     navigationActions: NavigationActions,
     locationViewModel: LocationViewModel,
     listActivitiesViewModel: ListActivitiesViewModel
 ) {
-  val context = LocalContext.current
-  val currentLocation by locationViewModel.currentLocation.collectAsState()
-  val coroutineScope = rememberCoroutineScope()
-  val activities by listActivitiesViewModel.uiState.collectAsState()
-  val defaultLocation = LatLng(46.519962, 6.633597) // EPFL
-  val firstToDoLocation =
-      try {
-        val loc =
-            (activities as ListActivitiesViewModel.ActivitiesUiState.Success)
-                .activities
-                .firstNotNullOf { it.location }
-        LatLng(loc.latitude, loc.longitude)
-      } catch (_: NoSuchElementException) {
-        defaultLocation
-      }
-  val cameraPositionState = rememberCameraPositionState {
-    position = CameraPosition.fromLatLngZoom(firstToDoLocation, 10f)
-  }
+    val context = LocalContext.current
+    val currentLocation by locationViewModel.currentLocation.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    val activities by listActivitiesViewModel.uiState.collectAsState()
+    val defaultLocation = LatLng(46.519962, 6.633597) // EPFL
+    var selectedActivity by remember { mutableStateOf<Activity?>(null) }
+    var showBottomSheet by remember { mutableStateOf(false) }
 
-  // Activity result launcher to request permissions
-  val locationPermissionLauncher =
-      rememberLauncherForActivityResult(
-          contract = ActivityResultContracts.RequestPermission(),
-          onResult = { isGranted ->
-            if (isGranted) {
-              locationViewModel.fetchCurrentLocation()
-            } else {
-              Log.d("MapScreen", "Location permission denied by the user.")
-            }
-          })
-
-  LaunchedEffect(Unit) {
-    if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
-        PackageManager.PERMISSION_GRANTED) {
-      locationViewModel.fetchCurrentLocation()
-    } else {
-      locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-    }
-  }
-
-  Scaffold(
-      content = { padding ->
-        Box(modifier = Modifier.fillMaxSize().padding(padding)) {
-          GoogleMap(
-              modifier = Modifier.fillMaxSize().padding(padding).testTag("mapScreen"),
-              cameraPositionState = cameraPositionState) {
+    val firstToDoLocation =
+        try {
+            val loc =
                 (activities as ListActivitiesViewModel.ActivitiesUiState.Success)
                     .activities
-                    .filter { it.location != null }
-                    .forEach { item ->
-                      Marker(
-                          state =
-                              MarkerState(
-                                  position =
-                                      LatLng(item.location!!.latitude, item.location!!.longitude)),
-                          title = item.title,
-                          snippet = item.description,
-                      )
-                    }
-                currentLocation?.let {
-                  Marker(
-                      state = rememberMarkerState(position = LatLng(it.latitude, it.longitude)),
-                      title = it.name,
-                      snippet = "Lat: ${it.latitude}, Lon: ${it.longitude}",
-                      icon =
-                          BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-                }
-              }
-
-          FloatingActionButton(
-              modifier =
-                  Modifier.align(Alignment.BottomStart)
-                      .padding(16.dp)
-                      .testTag("centerOnCurrentLocation"),
-              onClick = {
-                coroutineScope.launch {
-                  currentLocation?.let {
-                    val locationLatLng = LatLng(it.latitude, it.longitude)
-                    cameraPositionState.animate(
-                        update = CameraUpdateFactory.newLatLngZoom(locationLatLng, 15f),
-                        durationMs = 800)
-                  }
-                }
-              }) {
-                Icon(Icons.Default.MyLocation, contentDescription = "Center on current location")
-              }
+                    .firstNotNullOf { it.location }
+            LatLng(loc.latitude, loc.longitude)
+        } catch (_: NoSuchElementException) {
+            defaultLocation
         }
-      },
-      bottomBar = {
-        BottomNavigationMenu(
-            onTabSelect = { route -> navigationActions.navigateTo(route) },
-            tabList = LIST_TOP_LEVEL_DESTINATION,
-            selectedItem = Route.MAP)
-      })
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(firstToDoLocation, 10f)
+    }
+
+    // Activity result launcher to request permissions
+    val locationPermissionLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission(),
+            onResult = { isGranted ->
+                if (isGranted) {
+                    locationViewModel.fetchCurrentLocation()
+                } else {
+                    Log.d("MapScreen", "Location permission denied by the user.")
+                }
+            })
+
+    LaunchedEffect(Unit) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED) {
+            locationViewModel.fetchCurrentLocation()
+        } else {
+            locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    Scaffold(
+        content = { padding ->
+            Box(modifier = Modifier.fillMaxSize().padding(padding)) {
+                GoogleMap(
+                    modifier = Modifier.fillMaxSize().padding(padding).testTag("mapScreen"),
+                    cameraPositionState = cameraPositionState) {
+                    (activities as ListActivitiesViewModel.ActivitiesUiState.Success)
+                        .activities
+                        .filter { it.location != null }
+                        .forEach { item ->
+                            Marker(
+                                state =
+                                MarkerState(
+                                    position =
+                                    LatLng(item.location!!.latitude, item.location!!.longitude)),
+                                title = item.title,
+                                snippet = item.description,
+                                onClick = {
+                                    selectedActivity = item
+                                    showBottomSheet = true
+                                    true
+                                }
+                            )
+                        }
+                    currentLocation?.let {
+                        Marker(
+                            state = rememberMarkerState(position = LatLng(it.latitude, it.longitude)),
+                            title = it.name,
+                            snippet = "Lat: ${it.latitude}, Lon: ${it.longitude}",
+                            icon =
+                            BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                    }
+                }
+
+                FloatingActionButton(
+                    modifier =
+                    Modifier.align(Alignment.BottomStart)
+                        .padding(16.dp)
+                        .testTag("centerOnCurrentLocation"),
+                    onClick = {
+                        coroutineScope.launch {
+                            currentLocation?.let {
+                                val locationLatLng = LatLng(it.latitude, it.longitude)
+                                cameraPositionState.animate(
+                                    update = CameraUpdateFactory.newLatLngZoom(locationLatLng, 15f),
+                                    durationMs = 800)
+                            }
+                        }
+                    }) {
+                    Icon(Icons.Default.MyLocation, contentDescription = "Center on current location")
+                }
+            }
+        },
+        bottomBar = {
+            BottomNavigationMenu(
+                onTabSelect = { route -> navigationActions.navigateTo(route) },
+                tabList = LIST_TOP_LEVEL_DESTINATION,
+                selectedItem = Route.MAP)
+        })
+
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                // Ligne pour la croix en haut à droite
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween, // Espace entre les éléments
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Bouton "See more details" à gauche
+                    Button(
+                        onClick = {
+                            selectedActivity?.let { listActivitiesViewModel.selectActivity(it) }
+                            navigationActions.navigateTo(Screen.ACTIVITY_DETAILS)
+                            showBottomSheet = false
+                        },
+                    ) {
+                        Text("See more details")
+                    }
+
+                    // Icône de fermeture (croix) à droite
+                    IconButton(onClick = { showBottomSheet = false }) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Fermer",
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+                if (selectedActivity != null) {
+                    DisplayActivity(activity = selectedActivity!!)
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+
 }
+@Composable
+fun DisplayActivity(activity: Activity) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        // Image en haut
+        if (activity.images.isNotEmpty()) {
+            AsyncImage(
+                model = activity.images.first(),
+                contentDescription = "Image de l'activité",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(12.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+        }
+
+        // Titre et description
+        Text(
+            text = activity.title,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = activity.description,
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        // Date et localisation
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.CalendarToday,
+                contentDescription = "Date",
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Date: ${activity.date.toDate()}")
+        }
+        if (activity.location != null) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = "Lieu",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Lieu: ${activity.location!!.name}")
+            }
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+
+        // Prix et places disponibles
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Prix: ${activity.price}€",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "Places restantes: ${activity.placesLeft}/${activity.maxPlaces}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+
+        // Participants
+
+    }
+}
+
+
