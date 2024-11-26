6bac7325f679af24da960a2a8d59699c29375cdf
Amine Benjelloun Touimi <amine.benjellountouimi@epfl.ch>
2024-11-26 13:30:11 +0100

    refactor: add a constant for the context error message when phone is not connected.

--- app/src/main/java/com/android/sample/ui/components/OfflineAwareAction.kt
+++ app/src/main/java/com/android/sample/ui/components/OfflineAwareAction.kt
@@ -2,9 +2,6 @@ package com.android.sample.ui.components
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 import com.android.sample.model.network.NetworkManager
 import com.android.sample.resources.C.Tag.OFFLINE_TOAST_MESSAGE
 
@@ -14,9 +11,9 @@ fun performOfflineAwareAction(
     offlineMessage: String = OFFLINE_TOAST_MESSAGE,
     onPerform: () -> Unit
 ) {
-    if (networkManager.isNetworkAvailable()) {
-        onPerform()
-    } else {
-        Toast.makeText(context, offlineMessage, Toast.LENGTH_SHORT).show()
-    }
+  if (networkManager.isNetworkAvailable()) {
+    onPerform()
+  } else {
+    Toast.makeText(context, offlineMessage, Toast.LENGTH_SHORT).show()
+  }
 }
