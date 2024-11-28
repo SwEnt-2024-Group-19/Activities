2caa31c36e3c991512c95ed5453de306ea31a919
FreezDevo <mohamedtahaguelzim@gmail.com>
2024-11-28 21:39:16 +0100

    test: mock authResult to include a testUser
      * this could be enhanced to give a specific user depending on the credentials, and even fail the authentication attempt if credentials are considered invalid
    
      * change the two sign in methods (via email/via google)

--- app/src/androidTest/java/com/android/sample/model/authentication/MockSignInRepository.kt
+++ app/src/androidTest/java/com/android/sample/model/authentication/MockSignInRepository.kt
@@ -1,43 +1,32 @@
 package com.android.sample.model.authentication
 
-import android.util.Log
 import com.android.sample.model.auth.SignInRepository
-import com.android.sample.ui.navigation.NavigationActions
-import com.android.sample.ui.navigation.Screen
+import com.android.sample.resources.dummydata.testUser
 import com.google.firebase.auth.AuthResult
-import kotlinx.coroutines.delay
+import com.google.firebase.auth.FirebaseUser
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 
 class MockSignInRepository : SignInRepository {
 
   override suspend fun signInWithEmail(email: String, password: String): AuthResult {
-    // Simulate a delay and return a mocked AuthResult object
-    delay(500)
-    return Mockito.mock(AuthResult::class.java).apply {}
+    val authResult = Mockito.mock(AuthResult::class.java)
+    val firebaseUser = Mockito.mock(FirebaseUser::class.java)
+    `when`(firebaseUser.uid).thenReturn(testUser.id)
+    `when`(authResult.user).thenReturn(firebaseUser)
+
+    // Return a mocked AuthResult object with a test user
+    return authResult
   }
 
   override suspend fun signInWithGoogle(idToken: String): AuthResult {
-    // Simulate a delay and return a mocked AuthResult object
-    delay(500)
-    return Mockito.mock(AuthResult::class.java).apply {
-      // Configure any specific behavior if needed
-    }
-  }
+    val authResult = Mockito.mock(AuthResult::class.java)
+    val firebaseUser = Mockito.mock(FirebaseUser::class.java)
+    `when`(firebaseUser.uid).thenReturn(testUser.id)
+    `when`(authResult.user).thenReturn(firebaseUser)
 
-  override fun checkUserProfile(
-      uid: String?,
-      navigationActions: NavigationActions,
-      onAuthSuccess: () -> Unit,
-      onAuthError: (String) -> Unit
-  ) {
-    // Simulate the profile check logic
-    if (true) { // Will be correctly implemented when an e2e will test onAuthError
-      Log.d("MockSignInRepository", "User profile found")
-      onAuthSuccess()
-      navigationActions.navigateTo(Screen.OVERVIEW)
-    } else {
-      onAuthError("User profile not found")
-    }
+    // Return a mocked AuthResult object with a test user
+    return authResult
   }
 
   override fun signOut() {
