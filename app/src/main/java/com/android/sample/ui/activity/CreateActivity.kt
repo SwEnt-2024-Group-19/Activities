efbca06b84c6cd139975922c6fabeffc7e29a417
FreezDevo <mohamedtahaguelzim@gmail.com>
2024-12-05 14:01:55 +0100

    refactor: solve merge conflicts and remove unused imports

--- app/src/main/java/com/android/sample/ui/activity/CreateActivity.kt
+++ app/src/main/java/com/android/sample/ui/activity/CreateActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.HourglassTop
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -49,7 +48,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.PopupProperties
 import com.android.sample.R
 import com.android.sample.model.activity.Activity
