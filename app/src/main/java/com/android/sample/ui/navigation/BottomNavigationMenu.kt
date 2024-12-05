c678f00499d28a3c1ba3a6587c854586c8cc821f
ghaliberbich <ghali.berbich22@gmail.com>
2024-12-06 00:16:47 +0100

    style(): ktfmt format

--- app/src/main/java/com/android/sample/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/android/sample/ui/navigation/BottomNavigationMenu.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
