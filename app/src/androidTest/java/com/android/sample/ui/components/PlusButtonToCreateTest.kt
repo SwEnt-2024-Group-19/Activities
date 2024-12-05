f28e6ac4219ea00fba6cfc4bb706b265d16579f8
Yassyass181104 <yassine.alaoui@epfl.ch>
2024-12-05 22:08:53 +0100

    add:add tests for the new button

---
+++ app/src/androidTest/java/com/android/sample/ui/components/PlusButtonToCreateTest.kt
@@ -0,0 +1,47 @@
+package com.android.sample.ui.components
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.rememberNavController
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.navigation.TopLevelDestinations
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+
+class PlusButtonToCreateTest {
+
+    private lateinit var mockNavigationActions: NavigationActions
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        mockNavigationActions = Mockito.mock(NavigationActions::class.java)
+        composeTestRule.setContent {
+            PlusButtonToCreate(navigationActions = mockNavigationActions)
+        }
+    }
+
+    @Test
+    fun plusButtonToCreate_isDisplayed() {
+        composeTestRule.onNodeWithTag("plusRowToCreate").assertIsDisplayed()
+    }
+
+    @Test
+    fun plusButtonToCreate_performsClick() {
+        composeTestRule.onNodeWithTag("plusRowToCreate").performClick()
+        verify(mockNavigationActions).navigateTo(TopLevelDestinations.ADD_ACTIVITY)
+    }
+
+    @Test
+    fun plusButtonToCreate_iconIsDisplayed() {
+        composeTestRule.onNodeWithContentDescription(TopLevelDestinations.ADD_ACTIVITY.textId).assertIsDisplayed()
+    }
+}
