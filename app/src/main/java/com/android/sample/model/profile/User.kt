7a133a0c1858bef5546a7ef0f677ebbea2ca16c4
Amine Benjelloun Touimi <amine.benjellountouimi@epfl.ch>
2024-12-12 00:51:22 +0100

    feat(user): add categories to colors mapping for future usage and for coloring interests in profile creation.

--- app/src/main/java/com/android/sample/model/profile/User.kt
+++ app/src/main/java/com/android/sample/model/profile/User.kt
@@ -1,7 +1,14 @@
 package com.android.sample.model.profile
 
+import androidx.compose.ui.graphics.Color
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.android.sample.resources.C.Tag.ART_ACTIVITY_COLOR
+import com.android.sample.resources.C.Tag.CULTURAL_ACTIVITY_COLOR
+import com.android.sample.resources.C.Tag.INDOOR_COLOR
+import com.android.sample.resources.C.Tag.MUSICAL_ACTIVITY_COLOR
+import com.android.sample.resources.C.Tag.OUTDOOR_COLOR
+import com.android.sample.resources.C.Tag.SPORT_COLOR
 
 @Entity(tableName = "User")
 data class User(
@@ -24,3 +31,12 @@ val InterestCategories =
         "Cultural Activity",
         "Art Activity",
         "Musical Activity") + "None"
+val InterestCategoriesColors =
+    mapOf(
+        "Sport" to SPORT_COLOR,
+        "Outdoor Activity" to OUTDOOR_COLOR,
+        "Indoor Activity" to INDOOR_COLOR,
+        "Cultural Activity" to CULTURAL_ACTIVITY_COLOR,
+        "Art Activity" to ART_ACTIVITY_COLOR,
+        "Musical Activity" to MUSICAL_ACTIVITY_COLOR,
+        "None" to Color.Gray)
