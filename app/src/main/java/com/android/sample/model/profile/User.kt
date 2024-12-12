7b1580f8ed1b5059822c551d7ec38fe1a36732f8
FreezDevo <mohamedtahaguelzim@gmail.com>
2024-12-12 17:11:55 +0100

    refactor: readapt categories structure

--- app/src/main/java/com/android/sample/model/profile/User.kt
+++ app/src/main/java/com/android/sample/model/profile/User.kt
@@ -2,6 +2,8 @@ package com.android.sample.model.profile
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.android.sample.model.activity.Category
+import com.android.sample.model.activity.categories
 
 @Entity(tableName = "User")
 data class User(
@@ -14,13 +16,29 @@ data class User(
     val likedActivities: List<String>? = emptyList()
 )
 
-data class Interest(val category: String, val interest: String)
+data class Interest(val name: String, val category: Category)
 
-val InterestStringCategories =
-    listOf(
-        "Sport",
-        "Outdoor Activity",
-        "Indoor Activity",
-        "Cultural Activity",
-        "Art Activity",
-        "Musical Activity") + "None"
+val interestStringValues =
+    mapOf(
+        categories[0] to
+            listOf( // Sport
+                "Basketball", "Cycling", "Football", "Running", "Swimming", "Tennis", "Volleyball", "Yoga", "Other sports"
+                ),
+        categories[1] to
+            listOf( // Culture
+                "Art", "History", "Science", "Travel", "Other culture"
+                ),
+        categories[2] to
+            listOf( // Skills
+                "Cooking", "Dancing", "Programming", "Writing", "Drawing", "Photography", "Other skills"
+                ),
+        categories[3] to
+            listOf( // Entertainment
+                "Movies", "Music", "Reading", "TV Shows", "Video Games", "Other entertainment"
+                ))
+
+val interestsCategories = interestStringValues.keys.toList()
+val categoryOf =
+    interestStringValues
+        .flatMap { (category, interests) -> interests.map { it to category } }
+        .toMap()
