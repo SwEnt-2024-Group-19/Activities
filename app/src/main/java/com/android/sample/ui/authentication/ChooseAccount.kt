8aa71e19c77c1578b8f3c627a28ec019ab88cff9
Amine Benjelloun Touimi <amine.benjellountouimi@epfl.ch>
2024-12-14 11:36:52 +0100

    refactor(choose-account-screen): remove screen apparition when user already has a profile.

--- app/src/main/java/com/android/sample/ui/authentication/ChooseAccount.kt
+++ app/src/main/java/com/android/sample/ui/authentication/ChooseAccount.kt
@@ -1,21 +1,29 @@
 // ChooseAccount.kt
 package com.android.sample.ui.authentication
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
@@ -23,17 +31,20 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.android.sample.R
 import com.android.sample.model.auth.SignInViewModel
 import com.android.sample.model.image.ImageViewModel
 import com.android.sample.model.profile.ProfileViewModel
-import com.android.sample.resources.C.Tag.BUTTON_WIDTH
-import com.android.sample.resources.C.Tag.IMAGE_SIZE
-import com.android.sample.resources.C.Tag.LARGE_BUTTON_HEIGHT
+import com.android.sample.resources.C.Tag.BUTTON_HEIGHT_MD
+import com.android.sample.resources.C.Tag.CARD_ELEVATION_DEFAULT
+import com.android.sample.resources.C.Tag.HALF_SCREEN_TEXT_FIELD_PADDING
+import com.android.sample.resources.C.Tag.LINE_STROKE
 import com.android.sample.resources.C.Tag.MEDIUM_PADDING
+import com.android.sample.resources.C.Tag.ROUNDED_CORNER_SHAPE_DEFAULT
 import com.android.sample.resources.C.Tag.STANDARD_PADDING
 import com.android.sample.resources.C.Tag.SUBTITLE_FONTSIZE
 import com.android.sample.resources.C.Tag.TITLE_FONTSIZE
-import com.android.sample.ui.camera.ProfileImage
+import com.android.sample.resources.C.Tag.WIDTH_FRACTION_SM
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Screen
 
@@ -46,81 +57,93 @@ fun ChooseAccountScreen(
 ) {
   // Collect the user profile data from ProfileViewModel
   val userProfile by profileViewModel.userState.collectAsState()
-  val continueMessage =
-      if (userProfile != null) {
-        "Continue as ${userProfile?.name}"
-      } else "Complete profile creation"
+  if (userProfile != null) {
+    navigationActions.navigateTo(Screen.OVERVIEW)
+  }
+  val continueMessage = stringResource(R.string.complete_profile_creation_message)
+
+  Box(modifier = Modifier.fillMaxSize()) {
+    // Background Image
+    Image(
+        painter =
+            painterResource(
+                id = R.drawable.background_image), // Replace with your drawable resource
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize().alpha(0.2f),
+        contentScale = ContentScale.Crop, // Ensures the image fills the entire screen
+    )
+  }
   LazyColumn(
       modifier =
           Modifier.fillMaxSize()
               .padding(horizontal = MEDIUM_PADDING.dp)
               .testTag("chooseAccountScreen"),
       horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center) {
-        // Greeting Text
-        item {
-          Text(
-              text = "Hello ${userProfile?.name ?: "User"}, you are already signed in!",
-              fontSize = TITLE_FONTSIZE.sp,
-              fontWeight = FontWeight.Bold,
-              textAlign = TextAlign.Center,
-              modifier = Modifier.padding(bottom = MEDIUM_PADDING.dp).testTag("greetingText"),
-              overflow = TextOverflow.Ellipsis,
-              maxLines = 2)
-        }
+      verticalArrangement = Arrangement.Center,
+  ) {
+    // Greeting Text
+    item {
+      Text(
+          text = "Hello, you are already signed in!",
+          fontSize = TITLE_FONTSIZE.sp,
+          fontWeight = FontWeight.Bold,
+          textAlign = TextAlign.Center,
+          modifier = Modifier.padding(bottom = MEDIUM_PADDING.dp).testTag("greetingText"),
+          overflow = TextOverflow.Ellipsis,
+          maxLines = 2)
+    }
 
-        item { Spacer(modifier = Modifier.height(MEDIUM_PADDING.dp)) }
-
-        // Profile Image
-        item {
-          userProfile?.id?.let {
-            ProfileImage(
-                userId = it,
-                modifier = Modifier.size(IMAGE_SIZE.dp).clip(CircleShape).testTag("profilePicture"),
-                imageViewModel = imageViewModel)
+    item { Spacer(modifier = Modifier.height((2 * HALF_SCREEN_TEXT_FIELD_PADDING).dp)) }
+    // Continue Button
+    item {
+      Card(
+          modifier = Modifier.fillMaxWidth(WIDTH_FRACTION_SM).testTag("ChooseAccountCard"),
+          colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
+          elevation = CardDefaults.cardElevation(defaultElevation = CARD_ELEVATION_DEFAULT.dp),
+          shape = RoundedCornerShape(ROUNDED_CORNER_SHAPE_DEFAULT.dp)) {
+            Box(modifier = Modifier.fillMaxWidth().padding(horizontal = 0.dp, vertical = 0.dp)) {
+              OutlinedButton(
+                  onClick = {
+                    if (profileViewModel.userState.value != null) {
+                      navigationActions.navigateTo(Screen.OVERVIEW)
+                    } else navigationActions.navigateTo(Screen.CREATE_PROFILE)
+                  },
+                  shape = RoundedCornerShape(ROUNDED_CORNER_SHAPE_DEFAULT.dp),
+                  modifier =
+                      Modifier.height(BUTTON_HEIGHT_MD.dp).fillMaxWidth().testTag("continueText"),
+                  border = BorderStroke(LINE_STROKE.dp, Color.Transparent), // Transparent indicator
+                  colors =
+                      ButtonDefaults.buttonColors(
+                          containerColor = MaterialTheme.colorScheme.primary,
+                          contentColor = Color.White)) {
+                    Text(
+                        text = continueMessage,
+                        fontSize = SUBTITLE_FONTSIZE.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(vertical = STANDARD_PADDING.dp))
+                  }
+            }
           }
-        }
+    }
 
-        item { Spacer(modifier = Modifier.height(MEDIUM_PADDING.dp)) }
-        // Continue Button
-        item {
-          Button(
-              onClick = {
-                if (profileViewModel.userState.value != null) {
-                  navigationActions.navigateTo(Screen.OVERVIEW)
-                } else navigationActions.navigateTo(Screen.CREATE_PROFILE)
-              },
-              modifier =
-                  Modifier.width(BUTTON_WIDTH.dp)
-                      .height(LARGE_BUTTON_HEIGHT.dp)
-                      .clip(RoundedCornerShape(MEDIUM_PADDING.dp))
-                      .testTag("continueText")) {
-                Text(
-                    text = continueMessage,
-                    fontSize = SUBTITLE_FONTSIZE.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier.padding(vertical = STANDARD_PADDING.dp))
-              }
-        }
+    item { Spacer(modifier = Modifier.height(MEDIUM_PADDING.dp)) }
 
-        item { Spacer(modifier = Modifier.height(MEDIUM_PADDING.dp)) }
-
-        // Switch Account Button
-        item {
-          Text(
-              text = "Switch Account",
-              fontSize = SUBTITLE_FONTSIZE.sp,
-              color = Color.Blue,
-              fontWeight = FontWeight.SemiBold,
-              textDecoration = TextDecoration.Underline,
-              modifier =
-                  Modifier.padding(vertical = STANDARD_PADDING.dp)
-                      .clickable {
-                        signInViewModel.signOut()
-                        navigationActions.navigateTo(Screen.AUTH)
-                      }
-                      .testTag("switchAccountButton"))
-        }
-      }
+    // Switch Account Button
+    item {
+      Text(
+          text = stringResource(R.string.switch_account_message),
+          fontSize = SUBTITLE_FONTSIZE.sp,
+          color = Color.Blue,
+          fontWeight = FontWeight.SemiBold,
+          textDecoration = TextDecoration.Underline,
+          modifier =
+              Modifier.padding(vertical = STANDARD_PADDING.dp)
+                  .clickable {
+                    signInViewModel.signOut()
+                    navigationActions.navigateTo(Screen.AUTH)
+                  }
+                  .testTag("switchAccountButton"))
+    }
+  }
 }
