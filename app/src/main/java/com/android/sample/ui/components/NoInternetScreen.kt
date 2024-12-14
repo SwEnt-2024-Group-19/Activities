d2e67c7f8c4848226a88bb2d1e6a9e420515f7c1
Amine Benjelloun Touimi <amine.benjellountouimi@epfl.ch>
2024-12-14 10:32:25 +0100

    refactor(no-internet-screen): remove useless if condition from the code, answering a change requested.

--- app/src/main/java/com/android/sample/ui/components/NoInternetScreen.kt
+++ app/src/main/java/com/android/sample/ui/components/NoInternetScreen.kt
@@ -18,70 +18,61 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.sample.R
+import com.android.sample.resources.C.Tag.FADING_DURATION
 import com.android.sample.resources.C.Tag.IMAGE_SIZE
 import com.android.sample.resources.C.Tag.LARGE_PADDING
+import com.android.sample.resources.C.Tag.SPINNING_DURATION
 import com.android.sample.resources.C.Tag.TITLE_FONTSIZE
 
 @Composable
 fun NoInternetScreen() {
-  val isSpinning = true
-  val spinningDuration = 1500
-  val fadingDuration = 1000
   Box(
       modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.5f)),
       contentAlignment = Alignment.Center) {
         val logo = painterResource(id = R.drawable.aptivity)
-        if (isSpinning) {
-          val infiniteTransition = rememberInfiniteTransition(label = "")
+        val infiniteTransition = rememberInfiniteTransition(label = "")
 
-          // Animation for rotation
-          val rotation by
-              infiniteTransition.animateFloat(
-                  initialValue = 0f,
-                  targetValue = 360f,
-                  animationSpec =
-                      infiniteRepeatable(
-                          animation =
-                              tween(durationMillis = spinningDuration, easing = LinearEasing)),
-                  label = "")
+        // Animation for rotation
+        val rotation by
+            infiniteTransition.animateFloat(
+                initialValue = 0f,
+                targetValue = 360f,
+                animationSpec =
+                    infiniteRepeatable(
+                        animation =
+                            tween(durationMillis = SPINNING_DURATION, easing = LinearEasing)),
+                label = "")
 
-          // Animation for fading
-          val alpha by
-              infiniteTransition.animateFloat(
-                  initialValue = 0f,
-                  targetValue = 1f,
-                  animationSpec =
-                      infiniteRepeatable(
-                          animation = tween(durationMillis = fadingDuration, easing = LinearEasing),
-                          repeatMode = RepeatMode.Reverse),
-                  label = "")
+        // Animation for fading
+        val alpha by
+            infiniteTransition.animateFloat(
+                initialValue = 0f,
+                targetValue = 1f,
+                animationSpec =
+                    infiniteRepeatable(
+                        animation = tween(durationMillis = FADING_DURATION, easing = LinearEasing),
+                        repeatMode = RepeatMode.Reverse),
+                label = "")
 
-          Column(
-              horizontalAlignment = Alignment.CenterHorizontally,
-              verticalArrangement = Arrangement.Center,
-              modifier = Modifier) {
-                Image(
-                    painter = logo,
-                    contentDescription = "Loading Logo",
-                    modifier =
-                        Modifier.size((2 * IMAGE_SIZE).dp).graphicsLayer(rotationZ = rotation))
-                Spacer(modifier = Modifier.height(LARGE_PADDING.dp))
-                Text(
-                    text = stringResource(R.string.no_internet_connection),
-                    style =
-                        TextStyle(
-                            fontSize = TITLE_FONTSIZE.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Black // Ensure text is visible on the dark background
-                            ),
-                    modifier = Modifier.graphicsLayer(alpha = alpha) // Apply alpha animation
-                    )
-              }
-        } else {
-          Image(
-              painter = logo,
-              contentDescription = "Loading Logo",
-              modifier = Modifier.size(IMAGE_SIZE.dp))
-        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier) {
+              Image(
+                  painter = logo,
+                  contentDescription = "Loading Logo",
+                  modifier = Modifier.size((2 * IMAGE_SIZE).dp).graphicsLayer(rotationZ = rotation))
+              Spacer(modifier = Modifier.height(LARGE_PADDING.dp))
+              Text(
+                  text = stringResource(R.string.no_internet_connection),
+                  style =
+                      TextStyle(
+                          fontSize = TITLE_FONTSIZE.sp,
+                          fontWeight = FontWeight.Bold,
+                          color = Color.Black // Ensure text is visible on the dark background
+                          ),
+                  modifier = Modifier.graphicsLayer(alpha = alpha) // Apply alpha animation
+                  )
+            }
       }
 }
