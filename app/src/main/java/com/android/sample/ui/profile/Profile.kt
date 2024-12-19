c634fad78d7585c02c5ac6876b0f8b081e349b55
ghaliberbich <ghali.berbich22@gmail.com>
2024-12-19 20:18:56 +0100

    refactor(profile): remove the logs used in previous commit.

--- app/src/main/java/com/android/sample/ui/profile/Profile.kt
+++ app/src/main/java/com/android/sample/ui/profile/Profile.kt
@@ -3,7 +3,6 @@ package com.android.sample.ui.profile
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -207,14 +206,8 @@ fun ActivityRow(
         var bitmaps by remember { mutableStateOf(listOf<Bitmap>()) }
 
         LaunchedEffect(user) {
-          Log.d("ActivityRow", "Fetching images for activity ${activity.uid}")
           imageViewModel.fetchActivityImagesAsBitmaps(
-              activity.uid,
-              onSuccess = { urls ->
-                bitmaps = urls
-                Log.d("ActivityRow", "Fetched images successfully for activity ${activity.uid}")
-              },
-              onFailure = {})
+              activity.uid, onSuccess = { urls -> bitmaps = urls }, onFailure = {})
         }
 
         // Display image
